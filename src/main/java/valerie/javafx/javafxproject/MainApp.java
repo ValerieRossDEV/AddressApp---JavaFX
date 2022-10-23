c09b8c912e157ed55ad6963e725641c8b0b7ac11
Valerie <41258413+ValerieRossDEV@users.noreply.github.com>
2022-10-23 19:26:37 +0300

    functionality with edit window implemented

--- src/main/java/valerie/javafx/javafxproject/MainApp.java
+++ src/main/java/valerie/javafx/javafxproject/MainApp.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import valerie.javafx.javafxproject.model.Person;
 
@@ -42,7 +43,7 @@ public class MainApp extends Application {
     public ObservableList<Person> getPersonData() {
         return personData;
     }
-
+    @Override
     public void start(Stage primaryStage) {
         //1) Declare a primary stage (Everything will be on this stage)
         this.primaryStage = primaryStage;
@@ -72,19 +73,68 @@ public class MainApp extends Application {
     //Shows the person operations controller inside the root layout.
     public void showPersonOperationsView() {
         try {
-            //First, load PersonOperationsView from PersonOperations.fxml
+            //First, load PersonOperationsView from PersonOverview.fxml
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("PersonOperations.fxml"));
+            loader.setLocation(MainApp.class.getResource("PersonOverview.fxml"));
             AnchorPane employeeOperationsView = (AnchorPane) loader.load();
             // Set Person Operations controller into the center of root layout.
             rootLayout.setCenter(employeeOperationsView);
+
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     *
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
 
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
         launch();
--- src/main/java/valerie/javafx/javafxproject/PersonOverviewController.java
+++ src/main/java/valerie/javafx/javafxproject/PersonOverviewController.java
@@ -1,6 +1,7 @@
 package valerie.javafx.javafxproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -50,6 +51,13 @@ public class PersonOverviewController {
             // Initialize the person table with the two columns.
             firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
             lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+
+            // Clear person details.
+            showPersonDetails(null);
+
+            // Listen for selection changes and show the person details when changed.
+            personTable.getSelectionModel().selectedItemProperty().addListener(
+                    (observable, oldValue, newValue) -> showPersonDetails(newValue));
         }
 
         /**
@@ -91,4 +99,63 @@ public class PersonOverviewController {
             birthdayLabel.setText("");
         }
     }
+
+    /**
+     * Called when the user clicks on the delete button.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            personTable.getItems().remove(selectedIndex);
+        } else {
+            // Nothing selected.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+
+            alert.showAndWait();
+        }
+    }
+
+    /**
+     * Called when the user clicks the new button. Opens a dialog to edit
+     * details for a new person.
+     */
+    @FXML
+    private void handleNewPerson() {
+        Person tempPerson = new Person();
+        boolean okClicked = mainApp.showPersonEditDialog(tempPerson);
+        if (okClicked) {
+            mainApp.getPersonData().add(tempPerson);
+        }
+    }
+
+    /**
+     * Called when the user clicks the edit button. Opens a dialog to edit
+     * details for the selected person.
+     */
+    @FXML
+    private void handleEditPerson() {
+        Person selectedPerson = personTable.getSelectionModel().getSelectedItem();
+        if (selectedPerson != null) {
+            boolean okClicked = mainApp.showPersonEditDialog(selectedPerson);
+            if (okClicked) {
+                showPersonDetails(selectedPerson);
+            }
+
+        } else {
+            // Nothing selected.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+
+            alert.showAndWait();
+        }
+    }
+
 }
